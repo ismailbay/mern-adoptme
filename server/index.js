25a8235de0a2ee8a9195a09c2dd7628c9517e248
Ismail Bay <ismailbay@gmail.com>
2024-02-08 11:48:53 +0100

    feat: allow basic user registration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
