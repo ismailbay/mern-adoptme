086b19455bb5f0ac6d5d700cf42a1d081b9058e1
Ismail Bay <ismailbay@gmail.com>
2024-02-08 12:00:25 +0100

    feat: add error handler

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
-    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201);
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
