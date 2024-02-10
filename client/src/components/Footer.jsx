e14ab7f9f534149c1ceb4f64fd6b1aba5c41c1bb
Ismail Bay <ismailbay@gmail.com>
2024-02-10 20:19:25 +0100

    feat: add footer, make background darker

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,24 @@
+import { FaPaw } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="fixed bottom-0 left-0 z-20 w-full bg-sky-950 text-sky-100">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024{" "}
+          <a href="https://github.com/ismailbay" className="hover:underline">
+            AdoptME™
+          </a>
+          . Alle Rechte vorbehalten.
+        </span>
+        <ul className="flex flex-wrap items-end mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Impressum
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,24 @@
-import { FaSearch } from "react-icons/fa";
+import { FaPaw, FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-sky-100 shadow-md">
+    <header className="bg-sky-950 shadow-md text-sky-100">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-lg sm:text-3xl flex flex-wrap">
-            <span className="text-sky-400">Adopt</span>
-            <span className="text-sky-900">Me</span>
+          <h1 className="font-bold text-lg sm:text-3xl flex flex-wrap items-center">
+            <FaPaw className="text-sky-100" />
+            <span className="text-sky-200">Adopt</span>
+            <span className="text-sky-400">Me</span>
           </h1>
         </Link>
         <form className="bg-sky-50 p-2 rounded-md flex items-center">
           <input
             type="text"
-            className="bg-transparent focus:outline-none w-24 sm:w-96"
+            className="bg-transparent focus:outline-none w-24 sm:w-72"
             placeholder="Suche..."
           />
-          <FaSearch className="text-sky-300" />
+          <FaSearch className="text-sky-800" />
         </form>
         <ul className="flex gap-4">
           <Link to="/about">
